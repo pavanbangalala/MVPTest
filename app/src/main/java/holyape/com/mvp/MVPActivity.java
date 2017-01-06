b32d9fd9038dab98f473942e6a2336e653fd19d0
pavan <pawan.bangalala@gmail.com>
2017-01-06 17:34:43 +0530

    test

--- app/src/main/java/holyape/com/mvp/MVPActivity.java
+++ app/src/main/java/holyape/com/mvp/MVPActivity.java
@@ -10,4 +10,8 @@ public class MVPActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mvp);
     }
+
+    private void init(){
+        
+    }
 }
